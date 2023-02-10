367622023d6d38ff4866da220bd7ce614bebef02
Gabriel Charib <gabrielcharib@gmail.com>
2023-02-10 22:17:43 +0100

    create a list of 10 movies + a loop that runs through the movies
    todo :
    
    work more on the site disagne
    in the loop to be able to display the images of the films
    make a favorite list of the films
    
    to be able to increase the films thanks to an API of films

--- src/App.js
+++ src/App.js
@@ -6,14 +6,38 @@ import Film from "./components/Film";
 function App() {
   const [showMenu, setShowMenu] = useState(false);
   const [showMovies, setShowMovies] = useState(false);
-  const movies = [
-    { name: "The Shawshank Redemption", year: 1994 },
-    { name: "The Godfather", year: 1972 },
-    { name: "The Godfather: Part II", year: 1974 },
-    { name: "The Dark Knight", year: 2008 },
-    { name: "12 Angry Men", year: 1957 }
-  ];
-
+  const films = [
+    {
+      title: "The Shawshank Redemption",
+      description: "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
+      image: "https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"
+    },
+    {
+      title: "The Godfather",
+      description: "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
+      image: "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg"
+    },
+    {
+      title: "The Dark Knight",
+      description: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice.",
+      image: "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_.jpg"
+    },
+    {
+      title: "The Lord of the Rings: The Return of the King",
+      description: "Gandalf and Aragorn lead the World of Men against Sauron's army to draw his gaze from Frodo and Sam as they approach Mount Doom with the One Ring.",
+      image: "https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg"
+    },
+    {
+      title: "Pulp Fiction",
+      description: "The lives of two mob hitmen, a boxer, a gangster's wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
+      image: "https://m.media-amazon.com/images/M/MV5BMTkxMTA5OTAzMl5BMl5BanBnXkFtZTgwNjA5MDc3NjE@._V1_.jpg"
+    },
+    {
+      title: "The Good, the Bad and the Ugly",
+      description: "A bounty hunting scam joins two men in an uneasy alliance against a third in a race to find a fortune in gold buried in a remote cemetery.",
+      image: "https://m.media-amazon.com/images/M/MV5BOTQ"
+    }];
+  
   return (
     <div className="HomePage">
       <header className="header">
@@ -38,8 +62,8 @@ function App() {
               </a>
               {showMovies && (
                 <ul className="movies-list">
-                  {movies.map((movie, index) => (
-                    <li key={index}>{movie.name} ({movie.year})</li>
+                  {films.map((film, index) => (
+                    <li key={index}>{film.title} ({film.year})</li>
                   ))}
                 </ul>
               )}
@@ -59,6 +83,11 @@ function App() {
           Unlimited movies, TV shows, and more. Watch anywhere. Cancel anytime.
         </p>
         <button className="main-cta">Join Free for 30 Days</button>
+        {films.map((film, index) => (
+                    <><li key={index}>{film.title} ({film.year})</li><Film /></>
+                  ))}
+        <Film />
+        <Film />
       </main>
     </div>
   );
