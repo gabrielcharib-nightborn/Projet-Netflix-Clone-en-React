3ee8d747dbd1342875b0e267f4fbe1c48bc935b4
Gabriel Charib <gabrielcharib@gmail.com>
2023-02-11 14:52:52 +0100

    add the NetflixOriginal component

--- src/App.js
+++ src/App.js
@@ -155,6 +155,22 @@ function App() {
           </div>
           )
         }
+        const NetflixOriginal = () =>{
+          
+          return(
+            <div >
+            <h1>Netflix Originals</h1>
+            <div class="box">
+              <a href=""><img src="https://github.com/carlosavilae/Netflix-Clone/blob/master/img/o1.PNG?raw=true" alt=""></img></a>
+              <a href=""><img src="https://github.com/carlosavilae/Netflix-Clone/blob/master/img/o2.PNG?raw=true" alt=""></img></a>
+              <a href=""><img src="https://github.com/carlosavilae/Netflix-Clone/blob/master/img/o3.PNG?raw=true" alt=""></img></a>
+              <a href=""><img src="https://github.com/carlosavilae/Netflix-Clone/blob/master/img/o4.PNG?raw=true" alt=""></img></a>
+              <a href=""><img src="https://github.com/carlosavilae/Netflix-Clone/blob/master/img/o5.PNG?raw=true" alt=""></img></a>
+              <a href=""><img src="https://github.com/carlosavilae/Netflix-Clone/blob/master/img/o6.PNG?raw=true" alt=""></img></a>                
+            </div>
+            </div>
+          )
+        }
 
 
 
@@ -185,16 +201,16 @@ function App() {
   return (
   
     <div >
-      <Menu />
+        <Menu />
         <BackgroundVideo />
         <PromoBanner />    
         <PopularNetflix/>
         <TredingNow />
-        
-      <SocialIcon />
-      <Leslogos />
-      <SubLinks />
-      <Footer/>
+        <NetflixOriginal />
+        <SocialIcon />
+        <Leslogos />
+        <SubLinks />
+        <Footer/>
     </div>
     
   );
