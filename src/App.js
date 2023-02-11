a63fd62517e328451f39d25c6031a540430387dc
Gabriel Charib <gabrielcharib@gmail.com>
2023-02-11 12:50:04 +0100

    add the component SubLinks

--- src/App.js
+++ src/App.js
@@ -97,6 +97,48 @@ function App() {
           }
 
 
+          <div class="sub-links">
+          <ul>
+            <li><a href="#">Audio and Subtitles</a></li>
+            <li><a href="#">Audio Description</a></li>
+            <li><a href="#">Help Center</a></li>
+            <li><a href="#">Gift Cards</a></li>
+            <li><a href="#">Media Center</a></li>
+            <li><a href="#">Investor Relations</a></li>
+            <li><a href="#">Jobs</a></li>
+            <li><a href="#">Terms of Use</a></li>
+            <li><a href="#">Privacy</a></li>
+            <li><a href="#">Legal Notices</a></li>
+            <li><a href="#">Corporate Information</a></li>
+            <li><a href="#">Contact Us</a></li>
+          </ul>
+        </div>
+
+
+
+          const SubLinks = ()=> {
+            return (
+              <div class="sub-links">
+                  
+          <ul>
+            <li><a href="#">Audio and Subtitles</a></li>
+            <li><a href="#">Audio Description</a></li>
+            <li><a href="#">Help Center</a></li>
+            <li><a href="#">Gift Cards</a></li>
+            <li><a href="#">Media Center</a></li>
+            <li><a href="#">Investor Relations</a></li>
+            <li><a href="#">Jobs</a></li>
+            <li><a href="#">Terms of Use</a></li>
+            <li><a href="#">Privacy</a></li>
+            <li><a href="#">Legal Notices</a></li>
+            <li><a href="#">Corporate Information</a></li>
+            <li><a href="#">Contact Us</a></li>
+          </ul>
+        </div>
+            )
+          }
+
+
   
   return (
   
@@ -107,6 +149,7 @@ function App() {
         {films.map((film, index) => (
                     <><Film title={film.title}  poster={film.image} description={film.description}/></>
           ))}
+      <SubLinks />
       <Footer/>
     </div>
     
