094267bcc9e3cca97427982eab1c91b7f1eb6a8f
Gabriel Charib <gabrielcharib@gmail.com>
2023-02-25 15:49:15 +0100

    remake the menu using chackra components

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,22 +1,59 @@
-import React from "react";
+import { Box, Flex, IconButton, Image, Link } from "@chakra-ui/react";
+import { HamburgerIcon, SearchIcon, BellIcon } from "@chakra-ui/icons";
 
-function Menu (props){
-    return (
-   <div>
-      <nav className="main-nav"> 
-      <a id="logo" href="#home"><img src="https://github.com/carlosavilae/Netflix-Clone/blob/master/img/logo.PNG?raw=true" alt="Logo Image"></img></a>               
-        <a href="#home">Home</a>
-        <a href="#tvShows">TV Shows</a>
-        <a href="#movies">Movies</a>
-        <a href="#originals">Originals</a>
-        <a href="#">Recently   Added</a>
-        </nav> 
-      <nav class="sub-nav">
-      <a href="#"><i className="fas fa-search sub-nav-logo"></i></a>
-      <a href="#"><i className="fas fa-bell sub-nav-logo"></i></a>       
-      </nav>
-   </div>   
-    )
-    }
+function Menu() {
+  return (
+    <Flex justify="space-between" align="center" px={5} py={3}>
+      <Box>
+        <Link href="#home">
+          <Image
+            src="https://github.com/carlosavilae/Netflix-Clone/blob/master/img/logo.PNG?raw=true"
+            alt="Logo Image"
+          />
+        </Link>
+      </Box>
 
-    export default Menu;
+      <Box display={{ base: "none", md: "block" }}>
+        <Link href="#home" mr={4}>
+          Home
+        </Link>
+        <Link href="#tvShows" mr={4}>
+          TV Shows
+        </Link>
+        <Link href="#movies" mr={4}>
+          Movies
+        </Link>
+        <Link href="#originals" mr={4}>
+          Originals
+        </Link>
+        <Link href="#">Recently Added</Link>
+      </Box>
+
+      <Box display={{ md: "none" }}>
+        <IconButton
+          aria-label="Menu"
+          icon={<HamburgerIcon />}
+          variant="ghost"
+        />
+      </Box>
+
+      <Box display={{ base: "none", md: "block" }}>
+        <Flex align="center">
+          <IconButton
+            aria-label="Search"
+            icon={<SearchIcon />}
+            mr={4}
+            variant="ghost"
+          />
+          <IconButton
+            aria-label="Notifications"
+            icon={<BellIcon />}
+            variant="ghost"
+          />
+        </Flex>
+      </Box>
+    </Flex>
+  );
+}
+
+export default Menu;
